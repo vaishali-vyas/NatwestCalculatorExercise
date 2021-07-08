1236731208eb81709ed726151b5e9bee1b6aebd9
Vaishali <vvyas@qa.com>
2021-07-08 16:49:06 +0100

    all the changes

--- Flowchart2.java
+++ Flowchart2.java
@@ -4,7 +4,7 @@ public class Flowchart2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   
+  flowchartloop ();
 		
 		
 		
---
+++ Iterationflowchart1.java
@@ -0,0 +1,15 @@
+package calculatorExerciseone;
+
+public class Iterationflowchart1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 100; i <  200; i++) {
+	    	System.out.println(i+"A");
+	    }		
+		
+		
+	}
+
+}
